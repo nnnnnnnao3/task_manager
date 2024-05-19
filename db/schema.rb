28f1f6044e50c857d1f6bf837f61fba3b7173b3b
nnnnnnnao3 <nnnnnnnao3@gmail.com>
2024-05-19 21:52:51 +0900

    新規登録画面のhtml

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_05_17_034605) do
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
+    t.string "name", null: false
     t.text "profile"
     t.date "birthday"
     t.string "reset_password_token"
