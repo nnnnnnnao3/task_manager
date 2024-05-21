62c5ae9ad751d8b0a3370fae03bcfa5bb0d95393
nnnnnnnao3 <nnnnnnnao3@gmail.com>
2024-05-21 22:17:26 +0900

    task管理の導入

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,6 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @tasks = current_user.tasks
+  end
+end
---
+++ db/migrate/20240521062343_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string     :title,            null: false   
+      t.text       :descriptio
+      t.date       :due_date
+      t.references :user,             null: false,  foreign_key: true 
+      t.boolean    :completed, default: false # タスクの完了状態を表すブール型のカラム
+      t.timestamps
+    end
+  end
+end
