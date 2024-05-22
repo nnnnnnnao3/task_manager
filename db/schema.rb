119e06f277b129c42062cedf36790a2dd24dc38b
nnnnnnnao3 <nnnnnnnao3@gmail.com>
2024-05-22 20:41:06 +0900

    taskトップページ作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2024_05_21_062343) do
   create_table "tasks", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.text "descriptio"
-    t.date "due_date"
     t.bigint "user_id", null: false
-    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
